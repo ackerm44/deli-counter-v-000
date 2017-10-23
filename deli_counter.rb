f748a624f90175abdb7efba9277d52a02019abd1
Amanda Ackerman <ackerm44@gmail.com>
2017-10-23 23:12:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line(array)
 end
 
 def take_a_number(array, name)
-  array << name
+  array.push(name)
   number = array.index + 1
   puts "Welcome #{name}. You are number #{number} in line."
 end
