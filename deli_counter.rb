17e07922c7a556538b8657067dbb955616b6b798
Amanda Ackerman <ackerm44@gmail.com>
2017-10-23 23:01:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,15 @@ katz_deli = []
 def line(array)
   if array.size == 0
     puts "The line is currently empty."
-  else
-    puts "The line is currently #{array.index}. #{name}"
+  #else
+    #number = array.index + 1
+    #puts "The line is currently #{number}. #{name}"
   end
 end
+
+def take_a_number(array, name)
+  number = array.index + 1
+  puts "Welcome #{name}. You are number #{number} in line."
+end
+
+
