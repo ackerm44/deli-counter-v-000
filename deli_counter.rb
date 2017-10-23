0c9de7d7327c3aaae5a82ff85655313d5ea7b886
Amanda Ackerman <ackerm44@gmail.com>
2017-10-23 23:23:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
   if array.size == 0
     puts "The line is currently empty."
   else
-    string = "The line is currently "
+    string = "The line is currently: "
     number = 1
     array.each do |name|
       string.insert(-1, "#{number}. #{name} ")
